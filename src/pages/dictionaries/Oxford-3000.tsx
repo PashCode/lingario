d3c6815a6f8a2ee6187d2398ad76c66334d324c1
Yevhenii Pashko <y.pashcode@gmail.com>
2026-01-22 18:33:05 +0200

    refactor(dictionary): update Oxford-3000 page

--- src/pages/dictionaries/Oxford-3000.tsx
+++ src/pages/dictionaries/Oxford-3000.tsx
@@ -1,39 +1,7 @@
-import { useAppSelector } from "@/app/store";
-import { selectOxfordDictionary } from "@/features/dictionary/slice";
+import ShowDictionary from "@/features/dictionary/features/ShowDictionary";
 
 function Oxford3000() {
-  const oxfordDictionary = useAppSelector(selectOxfordDictionary);
-
-  return (
-    <div className="flex flex-col">
-      <h1>PUBLIC OXFORD 3000</h1>
-
-      <div>
-        {oxfordDictionary.map((word, index) => {
-          return (
-            <div key={index}>
-              <div className="flex">
-                <h1>
-                  <b>Слово:</b>&nbsp;
-                </h1>
-                <p>{word.e}</p>
-                &nbsp;
-                <h1>
-                  <b>Переклад:</b>&nbsp;
-                </h1>
-                <p>{word.u}</p>
-                &nbsp;
-                <h1>
-                  <b>Рівень:</b>&nbsp;
-                </h1>
-                <p>{word.l}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <ShowDictionary />;
 }
 
 export default Oxford3000;
